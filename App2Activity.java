49d38952039296a8b8c58e1aceea43333e4abf39
abellc16 <42623115+abellc16@users.noreply.github.com>
2018-10-06 19:24:31 -0400

    Update App2Activity.java

--- App2Activity.java
+++ App2Activity.java
@@ -7,11 +7,18 @@ import android.widget.Button;
 public class App2Activity extends Activity {
 
     Button button;
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final TextView instr = findViewById(R.id.instructionsText);
+        // final EditText ed1 = findViewById(R.id.usernameTextBox);
+
+        instr.setText("Welcome to our NFL statistics database. You can " +
+                "search by team, or by individual player. Swipe right to " +
+                "view league leaders. Enjoy!");
     }
 
 }
