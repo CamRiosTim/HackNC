8981ee9c6b7f4000adf3512c03340c43a9f60102
riosj16 <42673949+riosj16@users.noreply.github.com>
2018-10-06 21:25:08 -0400

    Update App2Activity.java

--- App2Activity.java
+++ App2Activity.java
@@ -2,23 +2,48 @@ package com.example.android.myfirst;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.*;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class App2Activity extends Activity {
+public class App2Activity extends Activity  implements AdapterView.OnItemSelectedListener {
 
     Button button;
-    
-     @Override
+    String[] users = { "Suresh Dasari", "Trishika Dasari", "Rohini Alavala", "Praveen Kumar", "Madhav Sai" };
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         final TextView instr = findViewById(R.id.instructionsText);
-        // final EditText ed1 = findViewById(R.id.usernameTextBox);
+        //final EditText ed1 = findViewById(R.id.usernameTextBox);
 
         instr.setText("Welcome to our NFL statistics database. You can " +
                 "search by team, or by individual player. Swipe right to " +
                 "view league leaders. Enjoy!");
+
+        setContentView(R.layout.activity_main);
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(), "Selected User: "+users[position] ,Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO - Custom Code
     }
 
 }
